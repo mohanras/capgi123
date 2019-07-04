c1274e3e1623a696364dd4f1333c5f02d04d028d
mohanras <mohanras@DIN78000323.corp.capgemini.com>
2019-07-04 16:32:52 +0530

    login done

---
+++ Sample/src/main/java/com/cg/log/Login.java
@@ -0,0 +1,20 @@
+package com.cg.log;
+
+public class Login {
+	private String uname;
+	private String pword;
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPword() {
+		return pword;
+	}
+	public void setPword(String pword) {
+		this.pword = pword;
+	}
+	
+
+}
---
+++ Sample/src/main/java/com/cg/pro/Process.java
@@ -0,0 +1,20 @@
+package com.cg.pro;
+
+public class Process {
+	int id;
+	String state;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+
+}
